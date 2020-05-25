db84ed5d8a91668a9d39f12a4421174ac0d9a38a
David Worsham <github@theworshams.com>
2020-05-25 15:00:32 -0500

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "DwSampleFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DwSampleFramework",
+            type: .dynamic,
             targets: ["DwSampleFramework"]),
     ],
     dependencies: [
