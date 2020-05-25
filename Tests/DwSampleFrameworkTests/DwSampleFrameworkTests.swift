e2598b08f8dda6723ba535051292bd9ff9c774c2
David Worsham <github@theworshams.com>
2020-05-25 15:52:39 -0500

    Added initial class and test

--- Tests/DwSampleFrameworkTests/DwSampleFrameworkTests.swift
+++ Tests/DwSampleFrameworkTests/DwSampleFrameworkTests.swift
@@ -1,15 +1,22 @@
 import XCTest
+import SwiftUI
 @testable import DwSampleFramework
 
 final class DwSampleFrameworkTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DwSampleFramework().text, "Hello, World!")
+  @State var text: String
+  
+  public init(text: String) {
+    self.text = text
+    super.init()
+  }
+  
+  func testExample() {
+    var body: some View {
+      DwSampleFramework.MainTextField(placeholder: "Testing", text: $text)
     }
+  }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  static var allTests = [
+      ("testExample", testExample),
+  ]
 }
